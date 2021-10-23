2dff9608359be94f94ed43770f518d725913ecca
Yaso-nanda <yasonanda111@gmail.com>
2021-10-23 12:47:18 +0530

    afa

--- src/main/java/tech/getarrays/employeemanager/resource/EmployeeResource.java
+++ src/main/java/tech/getarrays/employeemanager/resource/EmployeeResource.java
@@ -25,24 +25,28 @@ public class EmployeeResource {
     }
 
     @GetMapping("/find/{id}")
+    @CrossOrigin(origins = "http://192.168.50.104:4567/employeemanagerapp")
     public ResponseEntity<Employee> getEmployeeById (@PathVariable("id") Long id) {
         Employee employee = employeeService.findEmployeeById(id);
         return new ResponseEntity<>(employee, HttpStatus.OK);
     }
 
     @PostMapping("/add")
+    @CrossOrigin(origins = "http://192.168.50.104:4567/employeemanagerapp")
     public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee) {
         Employee newEmployee = employeeService.addEmployee(employee);
         return new ResponseEntity<>(newEmployee, HttpStatus.CREATED);
     }
 
     @PutMapping("/update")
+    @CrossOrigin(origins = "http://192.168.50.104:4567/employeemanagerapp")
     public ResponseEntity<Employee> updateEmployee(@RequestBody Employee employee) {
         Employee updateEmployee = employeeService.updateEmployee(employee);
         return new ResponseEntity<>(updateEmployee, HttpStatus.OK);
     }
 
     @DeleteMapping("/delete/{id}")
+    @CrossOrigin(origins = "http://192.168.50.104:4567/employeemanagerapp")
     public ResponseEntity<?> deleteEmployee(@PathVariable("id") Long id) {
         employeeService.deleteEmployee(id);
         return new ResponseEntity<>(HttpStatus.OK);
