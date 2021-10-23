70b2964ab8f127aa6119d0386a2be34b8dd784f7
Yaso-nanda <yasonanda111@gmail.com>
2021-10-23 13:10:53 +0530

    afafaf

--- src/main/java/tech/getarrays/employeemanager/resource/EmployeeResource.java
+++ src/main/java/tech/getarrays/employeemanager/resource/EmployeeResource.java
@@ -10,7 +10,6 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/employee")
-@CrossOrigin(origins = "http://192.168.50.104:4567")
 public class EmployeeResource {
     private final EmployeeService employeeService;
 
@@ -19,35 +18,30 @@ public class EmployeeResource {
     }
 
     @GetMapping("/all")
-    @CrossOrigin(origins = "http://192.168.50.104:4567/employeemanagerapp")
     public ResponseEntity<List<Employee>> getAllEmployees () {
         List<Employee> employees = employeeService.findAllEmployees();
         return new ResponseEntity<>(employees, HttpStatus.OK);
     }
 
     @GetMapping("/find/{id}")
-    @CrossOrigin(origins = "http://192.168.50.104:4567/employeemanagerapp")
     public ResponseEntity<Employee> getEmployeeById (@PathVariable("id") Long id) {
         Employee employee = employeeService.findEmployeeById(id);
         return new ResponseEntity<>(employee, HttpStatus.OK);
     }
 
     @PostMapping("/add")
-    @CrossOrigin(origins = "http://192.168.50.104:4567/employeemanagerapp")
     public ResponseEntity<Employee> addEmployee(@RequestBody Employee employee) {
         Employee newEmployee = employeeService.addEmployee(employee);
         return new ResponseEntity<>(newEmployee, HttpStatus.CREATED);
     }
 
     @PutMapping("/update")
-    @CrossOrigin(origins = "http://192.168.50.104:4567/employeemanagerapp")
     public ResponseEntity<Employee> updateEmployee(@RequestBody Employee employee) {
         Employee updateEmployee = employeeService.updateEmployee(employee);
         return new ResponseEntity<>(updateEmployee, HttpStatus.OK);
     }
 
     @DeleteMapping("/delete/{id}")
-    @CrossOrigin(origins = "http://192.168.50.104:4567/employeemanagerapp")
     public ResponseEntity<?> deleteEmployee(@PathVariable("id") Long id) {
         employeeService.deleteEmployee(id);
         return new ResponseEntity<>(HttpStatus.OK);
